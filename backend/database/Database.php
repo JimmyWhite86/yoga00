406887c663ddff358dde9f984dc68f1088dc9d35
JimmyWhite86 <andrea.bianchi086@gmail.com>
2025-12-02 17:08:59 +0100

    Add Database class for MySQL connection management

--- backend/database/Database.php
+++ backend/database/Database.php
@@ -1,1 +1,22 @@
 <?php
+
+class Database {
+    
+    // ATTRIBUTI
+    private $host = "localhost";
+    private $db_name = "yoga00";
+    private $username = "root";
+    private $password = "root";
+    private $conn;
+    
+    
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name.";charset=utf8", $this->username, $this->password);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
