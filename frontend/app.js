d976283a31c8194ba779b761216161bbe2eeffc0
JimmyWhite86 <andrea.bianchi086@gmail.com>
2025-12-17 12:09:39 +0100

    Login funziona anche con credenziali sbagliate
    Migliorata la funzione inviaRichiesta per gestire errori e risposte API

--- frontend/app.js
+++ frontend/app.js
@@ -19,7 +19,46 @@ const BASEURL = '../backend/api/';
 // @param {function} callback   - Funzione da eseguire con i dati ricevuti
 // @param {string} method       - Metodo HTTP (default: "GET")
 // @param {string} body         - Dati da inviare (JSON string, opzionale)
-function inviaRichiesta(api, callback, method = "GET", body) {
+function inviaRichiesta (api, callback, method = 'GET', body = null) {
+    fetch(BASEURL + api, {
+        method,
+        headers: body ? {'Content-Type': 'application/json'} : undefined,
+        body,
+        credentials: 'include'
+    })
+        .then(response=> {
+            // Leggo JSON anche se è presente un errore
+            return response.json().then(data => ({
+                ok: response.ok,
+                status: response.status,
+                data: data
+            }));
+        })
+        .then(result => {
+            if (result.ok) {
+                // Non ci sono errori => eseguo la callback normale
+                callback(result.data);
+            } else {
+                // In caso di errori HTTP
+                const msg = result.data.messagio || 'Errore sconosciuto';
+                console.error(`Errore dalle API: ${result.status}: ${msg}`);
+
+                // Mostro messaggio all'utente
+                alert(`Errore: ${msg}`);
+
+                // TODO: Cambiare alert ed eventualmente preparare una callback ad hoc
+            }
+        })
+        .catch(error => {
+            console.error("Errore: ", error);
+            alert ("Errore di connessione al server");
+            // TODO: modificare alert con qualcosa di piu elegante.
+        })
+}
+
+
+
+function inviaRichiestaOLD(api, callback, method = "GET", body) {
   const fetchPromise = fetch(BASEURL + api, {
     method,
     headers: body ? {'Content-Type': 'application/json'} : undefined,
